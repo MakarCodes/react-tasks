c10ce2fb6ea5ed85f138bd14181f43bb2577b00e
MakarCodes <jarek.strupek@wp.pl>
2020-11-24 13:46:51 +0100

    Added test to taskOne

--- src/Tasks/Task_1/__tests__/useSetState.test.tsx
+++ src/Tasks/Task_1/__tests__/useSetState.test.tsx
@@ -2,7 +2,7 @@ import { renderHook, act } from '@testing-library/react-hooks';
 import useSetState from '../useSetState';
 
 describe('useSetState hook works correctly if', () => {
-  // it('throws an error when given payload in invalid', () => {
+  // it('throws an error when given payload is invalid', () => {
   //   const initialState = { name: 'Jarek', age: 28 };
   //   try {
   //     const {result} = renderHook(() => useSetState(initialState));
@@ -20,5 +20,34 @@ describe('useSetState hook works correctly if', () => {
     const { result } = renderHook(() => useSetState(initialState));
     expect(result.current[0]).toEqual(initialState);
     expect(result.current[1]).toBeInstanceOf(Function);
+  });
+  it('stateUpdater function works correctly for given partial state to update', () => {
+    const initialState = { name: 'Jarek', age: 28 };
+    const { result } = renderHook(() => useSetState(initialState));
+    const stateUpdater = result.current[1];
+    act(() => {
+      stateUpdater({ name: 'Janek' });
+      stateUpdater({ gender: 'male' });
+    });
+    expect(result.current[0]).toEqual({
+      name: 'Janek',
+      age: 28,
+      gender: 'male',
+    });
+  });
+  it('stateUpdater function works correctly when given payload has partial state and callback function', () => {
+    const initialState = { name: 'Jarek', age: 28 };
+    const { result } = renderHook(() => useSetState(initialState));
+    const stateUpdater = result.current[1];
+    act(() => {
+      stateUpdater({ name: 'Janek' }, () => {
+        stateUpdater({ gender: 'male' });
+      });
+    });
+    expect(result.current[0]).toEqual({
+      name: 'Janek',
+      age: 28,
+      gender: 'male',
+    });
   });
 });
